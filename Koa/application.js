0df0ff8f495d9f360237a90015cc8eb674162a0d
aihao <1185491736@qq.com>
2020-05-31 11:00:17 +0800

    feat: Koa整体框架

---
+++ Koa/application.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const EventEmitter = require('event');
+
+class Koa extends EventEmitter {
+  constructor() {
+    super()
+  }
+  use() {
+
+  }
+  listen() {
+
+  }
+}
+
+module.exports = Koa;
